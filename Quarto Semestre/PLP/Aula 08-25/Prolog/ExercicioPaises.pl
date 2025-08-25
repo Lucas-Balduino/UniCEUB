879e2c85edc963de8d23b4426af6d1883e0a9722
Lucas Gonçalves Balduino <lucas.balduino@dinfor.uniceub>
2025-08-25 15:47:35 -0300

    Aula PLL 25-08

---
+++ Quarto Semestre/PLP/Aula 08-25/Prolog/ExercicioPaises.pl
@@ -0,0 +1,14 @@
+pais(brasil,8.51,212).
+pais(espanha,0.50603,48.81).
+pais(portugal,0.092152,10.7).
+pais(uzbequistao,0.4489,36.36).
+
+
+/*
+Formas de Consulta:
+ -> "\=": variáveis sao diferentes
+ -> "@<": Consultas nao retroativas (nao repete os paises no x1 e x2)
+ -> pais(X1,A1,P1),pais(X2,A2,P2),Dif is P1 - P2, X1 \= X2.:
+    Criação de variaveis que realizam operacoes aritiméticas com os
+    dados para trazer mais informações.
+*/

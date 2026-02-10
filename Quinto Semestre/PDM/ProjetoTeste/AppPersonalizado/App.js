9375c901d372c21b2462a98ed593778fa561a5e5
Lucas Gonçalves Balduino <lucas.balduino@dinfor.uniceub>
2026-02-10 16:38:52 -0300

    Aula PDM

---
+++ Quinto Semestre/PDM/ProjetoTeste/AppPersonalizado/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.Text}>Bem Vindo ao Meu App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0004',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Text:{
+    color: '',
+    fontFamily: 'Arial',
+  },
+});
---
+++ Quinto Semestre/PDM/ProjetoTeste/ProcessoDeCriação.md
@@ -0,0 +1,13 @@
+"npx create-expo-app@latest"
+- Definição do nome
+
+
+"npx create-expo-app@latest AppPersonalizado -t"
+// Criação de App com nome e personalização de template
+
+"cd NomeDoApp"
+Direcionar para o diretorio do projeto
+
+"expo start"
+
+// Em projetos JS Personalizados instalar pacotes do REACT com npx expo install "react-dom react-native-web"
